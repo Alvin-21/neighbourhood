904d14a25cc1956394eeed80efb61b51586607f4
Alvin Awuor <alvinawuor8@gmail.com>
2021-06-05 13:04:01 +0300

    Add user model.

--- hood/models.py
+++ hood/models.py
@@ -8,4 +8,13 @@ class Neighbourhood(models.Model):
     hood_name = models.CharField(max_length=50)
     hood_location = models.CharField(max_length=50)
     occupants = models.PositiveIntegerField(default=0)
-    admin = models.ForeignKey(User, is_staff=True)
+    admin = models.ForeignKey(User, on_delete=models.CASCADE, is_staff=True)
+
+
+class User(models.Model):
+    image = CloudinaryField('image', null=True)
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    neighbourhood_id = models.ForeignKey(Neighbourhood, on_delete=models.CASCADE)
+    email = models.EmailField()
+    bio = models.CharField(max_length=200)
