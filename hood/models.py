758c912d0ce487db5220e8525904c536e2deb286
Alvin Awuor <alvinawuor8@gmail.com>
2021-06-05 12:56:28 +0300

    Add neighbourhood model.

--- hood/models.py
+++ hood/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from cloudinary.models import CloudinaryField
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Neighbourhood(models.Model):
+    hood_name = models.CharField(max_length=50)
+    hood_location = models.CharField(max_length=50)
+    occupants = models.PositiveIntegerField(default=0)
+    admin = models.ForeignKey(User, is_staff=True)
