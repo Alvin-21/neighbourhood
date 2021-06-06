b4de4e1bccacbd60b3c685a05a5f01f1d6d8bf05
Alvin Awuor <alvinawuor8@gmail.com>
2021-06-06 23:31:21 +0300

    Add url for editing profile form.

--- hood/urls.py
+++ hood/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     re_path(r'^new/neighbourhood$', views.create_hood, name='new_neighbourhood'),
     re_path(r'^new/business/hood-id/(\d+)$', views.create_business, name='new_business'),
     re_path(r'^new/post/hood-id/(\d+)$', views.create_post, name='new_post'),
+    re_path(r'^edit/profile/(\d+)$', views.edit_profile, name='edit_profile'),
 ]
