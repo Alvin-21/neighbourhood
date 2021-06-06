d09cdc4ef7eda3f6c33bd101c64b8341ad21aa6d
Alvin Awuor <alvinawuor8@gmail.com>
2021-06-06 16:39:55 +0300

    Add form for business.

--- hood/forms.py
+++ hood/forms.py
@@ -1,2 +1,9 @@
 from django import forms
 from .models import *
+
+class BusinessForm(forms.ModelForm):
+    class Meta:
+        model = Business
+        exclude = ['user', 'neighbourhood_id']
+
+
