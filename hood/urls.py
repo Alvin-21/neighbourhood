c17bc663070a949f4fee8aefa39b6e8e03fe38de
Alvin Awuor <alvinawuor8@gmail.com>
2021-06-06 22:36:39 +0300

    Add url for new hood form.

--- hood/urls.py
+++ hood/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     re_path(r'^$', views.index, name='homepage'),
     re_path(r'^hood/(\d+)', views.hood, name='hood'),
     re_path(r'^profile/(\d+)', views.profile, name='profile'),
+    re_path(r'^new/neighbourhood$', views.create_hood, name='new_neighbourhood'),
 ]
