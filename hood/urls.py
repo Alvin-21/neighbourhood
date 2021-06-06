9cd318f0eaa6dba57bc3203f6b4dcbd724060f41
Alvin Awuor <alvinawuor8@gmail.com>
2021-06-06 23:17:07 +0300

    Add url for new post form.

--- hood/urls.py
+++ hood/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     re_path(r'^profile/(\d+)', views.profile, name='profile'),
     re_path(r'^new/neighbourhood$', views.create_hood, name='new_neighbourhood'),
     re_path(r'^new/business/hood-id/(\d+)$', views.create_business, name='new_business'),
+    re_path(r'^new/post/hood-id/(\d+)$', views.create_post, name='new_post'),
 ]
